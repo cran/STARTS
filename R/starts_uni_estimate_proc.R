5e28149d3e398c341169705a0508120660b884ce
Alexander Robitzsch <robitzsch@ipn.uni-kiel.de>
2018-03-21 14:00:15 +0000

    version 0.3-24

--- R/starts_uni_estimate_proc.R
+++ R/starts_uni_estimate_proc.R
@@ -1,52 +1,69 @@
 ## File Name: starts_uni_estimate_proc.R
-## File Version: 0.04
+## File Version: 0.25
 
 starts_uni_estimate_proc <- function( data, time_index, covmat, pars_inits , nobs, 
 		est_var_trait,	est_var_ar, est_var_state, var_meas_error )
 {
 	#--- data processing
 	data0 <- data
+	some_missings <- FALSE
+	suff_stat <- NULL
 	# handle data input
-	if ( ! is.null(data) ){
+	if ( ! is.null(data) ){		
+		nobs <- nrow(data)
 		if ( sum( is.na(data) ) > 0 ){
-			warning("Function 'starts_uni_estimate' uses listwise deletion to handle missing data.\n")	
+			some_missings <- TRUE
+			suff_stat <- LAM::suff_stat_NA_pattern(data) 
+			# suff_stat <- starts_uni_estimate_proc_modify_suff_stat( suff_stat, var_meas_error)
 		}
 		data <- stats::na.omit(data)
-		covmat <- stats::cov(data)			
+		covmat <- stats::cov(data)		
+		M <- colMeans(data0, na.rm=TRUE)
+		W <- ncol(data)
+	} else {
+		M <- rep(0, ncol(covmat) )
+		W <- ncol(covmat)
 	}
-	diag(covmat) <- diag(covmat) - var_meas_error
-	
-	
+	if ( ! is.null(var_meas_error) ){
+		if ( is.matrix(var_meas_error) | is.data.frame(var_meas_error) ){
+			var_meas_error <- as.numeric( var_meas_error[1,,drop=TRUE] )
+		}		
+	}	
+	# diag(covmat) <- diag(covmat) - var_meas_error	
 	if (! is.null(time_index)){
 		time_index <- cumsum( c(0, diff(time_index)) ) + 1
 	}
 	#--- average standard deviation
-	sd0 <- mean( sqrt( diag(covmat) ) )	
-	
+	if ( ! some_missings){
+		sd0 <- mean( sqrt( diag(covmat) - var_meas_error )  )	
+	} else {
+		sd0 <- mean( apply( data0 , 2 , stats::sd , na.rm=TRUE ) )
+	}
 	#--- estimated parameters
-	par_names <- c("var_trait", "var_ar", "var_state", "a")	
-	pars_est <- starts_vector_with_names( c( est_var_trait,	est_var_ar, est_var_state , TRUE ) , par_names )	
+	par_names <- c( paste0("mu",1:W), c("var_trait", "var_ar", "var_state", "a") )
+	est_pars <- c( rep(TRUE, W) , c( est_var_trait,	est_var_ar, est_var_state , TRUE )	)
+	pars_est <- starts_vector_with_names( est_pars , par_names )	
 	if ( ! est_var_ar ){
 		pars_est["a"] <- FALSE
 	}
 	
 	#--- define initial parameters	
 	if ( is.null(pars_inits) ){
-		pars_inits <- starts_vector_with_names( c( sd0*c( .33, .33, .33 ),.5), par_names )				
+		par1 <- c( M, c( sd0*c( .33, .33, .33 ),.5) )
+		pars_inits <- starts_vector_with_names( par1 , par_names )				
 	}
 	pars_inits <- pars_inits[ pars_est ]
 	par_names_est <- par_names[ pars_est ]
-	
-	W <- ncol(covmat)
+		
 	#-- degrees of freedom SEM
 	covmat_np <- W*(W-1)/2 + W
 	estimated_np <- est_var_trait + est_var_ar + est_var_state + 1
 	df_sem <- covmat_np - estimated_np
 	
-	
 	#----- output
-	res <- list(W=W, pars_inits=pars_inits, time_index=time_index, covmat=covmat, data0=data0,
+	res <- list(W=W, pars_inits=pars_inits, time_index=time_index, covmat=covmat, M=M, data0=data0,
 				par_names=par_names, nobs = nobs, sd0 = sd0, pars_est=pars_est, df_sem=df_sem,
-				par_names_est=par_names_est)
+				par_names_est=par_names_est, some_missings=some_missings, suff_stat=suff_stat,
+				var_meas_error=var_meas_error)
 	return(res)
 }
--- R/summary.starts_uni.R
+++ R/summary.starts_uni.R
@@ -1,8 +1,8 @@
 ## File Name: summary.starts_uni.R
-## File Version: 0.18
+## File Version: 0.26
 
 #############################################################
-summary.starts_uni <- function( object , digits=3 , file=NULL ,... )
+summary.starts_uni <- function( object , digits=3 , file=NULL, print_call=TRUE, ... )
 {
     # open sink
     CDM::osink( file = file , suffix = paste0( "__SUMMARY.Rout") )
@@ -20,7 +20,9 @@ summary.starts_uni <- function( object , digits=3 , file=NULL ,... )
 	cat( "Used function =" , object$used_function , "\n\n" )
 	
 	#-- print call
-	sirt::sirt_summary_print_call(CALL=object$CALL)				
+	if (print_call){
+		sirt::sirt_summary_print_call(CALL=object$CALL)				
+	}
 	   
 	#-- print computation time
 	starts_summary_print_computation_time(object=object)
@@ -73,20 +75,31 @@ summary.starts_uni <- function( object , digits=3 , file=NULL ,... )
 		obji$effSize <- round( obji$effSize )		
 	}			
 	sirt::sirt_summary_print_objects(obji=obji, from=2, digits=3, rownames_null=TRUE)	
-	
-	
+
 	cat("-----------------------------------------------------------------\n")
-	cat("Model Fit \n\n")	
+	cat("Variance Proportions \n")	
 
-	digits_fit <- 3
+	if (object$use_pmle){ 
+		obji <- object$var_prop
+	}
+	if (object$use_amh){ 
+		obji <- object$var_prop
+		obji <- obji[, intersect(vars, colnames(obji)) ]
+		obji$effSize <- round( obji$effSize )		
+	}			
+	sirt::sirt_summary_print_objects(obji=obji, from=2, digits=3, rownames_null=TRUE)	
+			
+	if ( ! object$some_missings ){
+		cat("-----------------------------------------------------------------\n")
+		cat("Model Fit \n\n")	
+		digits_fit <- 3
+		cat( paste0("Chi square test of model fit: \nChi2(df=", object$model_fit$df_sem , 
+					")=" , round( object$model_fit$chisq , digits_fit ) , ", p=" , 
+					round( object$model_fit$p_chisq , digits_fit ) , "\n\n") )
+		cat( "SRMR =" , round( object$model_fit$SRMR , digits_fit ) , "\n" )  
+		cat( "RMSEA =" , round( object$model_fit$RMSEA , digits_fit ) , "\n" )  
+	}
 
-	cat( paste0("Chi square test of model fit: \nChi2(df=", object$model_fit$df_sem , 
-				")=" , round( object$model_fit$chisq , digits_fit ) , ", p=" , 
-				round( object$model_fit$p_chisq , digits_fit ) , "\n\n") )
-	cat( "SRMR =" , round( object$model_fit$SRMR , digits_fit ) , "\n" )  
-	cat( "RMSEA =" , round( object$model_fit$RMSEA , digits_fit ) , "\n" )  
-	
-	invisible(obji)
 	# close sink
     CDM::csink( file = file )		
 	
